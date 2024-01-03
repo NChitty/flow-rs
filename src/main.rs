2b72b33d1c815cf633abcb881244f5ba3783870d
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-01-03 07:38:12 -0500

    REPL with quit

--- src/main.rs
+++ src/main.rs
@@ -14,31 +14,88 @@
  * limitations under the License.
  */
 
-use clap::{Args, Parser, ValueEnum};
+use std::io;
+use std::io::Write;
+use clap::{ArgMatches, Args, CommandFactory, Error, Parser, Subcommand, ValueEnum};
 
-#[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None, multicall = true)]
 struct Cli {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Debug, Subcommand)]
+enum Action {
+    /// load into memory
+    Read(Arguments),
+    /// exit the program
+    Quit
+}
+
+#[derive(Args, Debug)]
+struct Arguments {
     /// The type of logical artifact to operate on
     #[arg(value_enum, required = true)]
     r#type: ArtifactType,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd, ValueEnum)]
 enum ArtifactType {
     /// apply action to a binary decision diagram
     #[value(name = "bdd")]
     BinaryDecisionDiagram,
     /// apply action to crossbar matrix
     #[value(name = "xbar")]
-    CrossbarMatrix
+    CrossbarMatrix,
 }
 
-fn main() {
-    let cli = Cli::parse();
+fn main() -> Result<(), String> {
+    loop {
+        let line = read_line()?;
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
 
-    match cli.r#type {
-        ArtifactType::BinaryDecisionDiagram => println!("Operating on bdd."),
-        ArtifactType::CrossbarMatrix => println!("Operating on crossbar.")
+        let cli = parse_command(line);
+        match cli {
+            Some(command) => if respond(command)? {
+                return Ok(());
+            },
+            None => {
+                continue;
+            }
+        }
+    }
+}
+
+fn respond(command: Cli) -> Result<bool, String> {
+    match command.action {
+        Action::Read(_) => {
+            println!("TODO");
+            Ok(false)
+        },
+        Action::Quit => Ok(true)
+    }
+}
+
+fn read_line() -> Result<String, String> {
+    write!(io::stdout(), "$ ").map_err(|e| e.to_string())?;
+    io::stdout().flush().map_err(|e| e.to_string())?;
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .map_err(|e| e.to_string())?;
+    Ok(buffer)
+}
+
+fn parse_command(line: &str) -> Option<Cli> {
+    match Cli::try_parse_from(line.split_ascii_whitespace()) {
+        Ok(cli) => Some(cli),
+        Err(e) => {
+            e.print().unwrap();
+            None
+        }
     }
 }
