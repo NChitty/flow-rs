b56039ef9dd15ca357d94c7e9d4f56dd95a2d7f2
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-01-05 21:05:38 -0500

    Format

--- src/bdd/display.rs
+++ src/bdd/display.rs
@@ -1,20 +1,21 @@
 /*
- *    Copyright (c) 2024 William Nicholas Chitty
+ * Copyright (c) 2024 William Nicholas Chitty
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 use std::fmt::{Display, Formatter};
+
 use crate::bdd::{BinaryDecisionDiagram, BinaryNode, DecisionNode};
 
 impl Display for BinaryDecisionDiagram {
@@ -42,7 +43,11 @@ impl Display for BinaryNode {
 
 impl Display for DecisionNode {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} {} {}", self.decision_map[1], self.decision_map[0], self.variable_id)
+        write!(
+            f,
+            "{} {} {}",
+            self.decision_map[1], self.decision_map[0], self.variable_id
+        )
     }
 }
 
