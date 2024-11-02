266626000c4332e73c9452a94aa83ed9292c1e58
Nicholas Chitty <nicholas.chitty@gmail.com>
2024-11-02 10:54:20 -0400

    code organization

--- src/bdd/mod.rs
+++ src/bdd/mod.rs
@@ -63,3 +63,32 @@ impl DecisionNode {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::bdd::DecisionNode;
+
+    #[test]
+    fn given_unassigned_variable_then_error() {
+        let var = None;
+        let node = DecisionNode::new_node(0, 0, 0);
+
+        assert!(node.evaluate(var).err().is_some());
+    }
+
+    #[test]
+    fn given_false_variable_then_false_node() {
+        let var = Some(false);
+        let node = DecisionNode::new_node(1, 0, 0);
+
+        assert_eq!(1, node.evaluate(var).unwrap());
+    }
+
+    #[test]
+    fn given_true_variable_then_true_node() {
+        let var = Some(true);
+        let node = DecisionNode::new_node(0, 1, 0);
+
+        assert_eq!(1, node.evaluate(var).unwrap());
+    }
+}
